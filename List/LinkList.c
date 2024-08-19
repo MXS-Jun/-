1adce353f130a19e0771755770fda1a3f521a85e
MXS-Jun <bjtuzj0328@gmail.com>
2024-08-19 13:38:58 +0800

    写了单链表的结构体定义

---
+++ List/LinkList.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef int ElemType;
+
+typedef struct Node
+{
+    ElemType data;
+    struct Node *next;
+} Node;
+
+typedef struct Node *LinkList;
+
+int main(void)
+{
+
+    return 0;
+}
